a456374cceb0a9c1bdd242b29e3f4f8d8a90ee22
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-08-19 01:06:11 -0700

    prep

---
+++ _printf.h
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * _printf - a function that works like "printf" form the std library
+ * @format: a string to write with specifier
+ * Return: the number of char prindted (excluding NULL byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list format;
+
+	if (!format)
+		return (-1);
+}
---
+++ print_char.h
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_char - prints char
+ * @c: the char to be printed
+ * Return: void
+ */
+void print_char(char c)
+{
+	/*need to use write function*/
+}
